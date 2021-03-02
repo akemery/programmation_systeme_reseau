c8a3ed9c5378d3646da3d976c84027ccd1301403
Emery Kouassi Assogba <assogba.emery@gmail.com>
2021-03-02 21:31:41 +0100

    manipulation de repertoire et correction exo 4

--- test_exo4_post.c
+++ test_exo4_post.c
@@ -6,12 +6,12 @@ int main(int argc, char *argv[]){
   int i;
   pid_t pid[4];
   int fd[4][2], ret;
-  ret = pipe(fd);
-  if(ret < 0){
-    fprintf(stderr,"Erreur de création du pipe (%d)\n", errno);
-    return 1;
-  }
   for(i = 0; i < 4; i++){
+    ret = pipe(fd[i]);
+    if(ret < 0){
+      fprintf(stderr,"Erreur de création du pipe (%d)\n", errno);
+      return 1;
+    }
     pid[i] = fork();
     if(pid[i]<0){
       /*On gère l'erreur*/
@@ -22,12 +22,12 @@ int main(int argc, char *argv[]){
       char buffer[1];
       /* Dans le fils*/
       fprintf(stdout, "Je suis (%d), mon père (%d)\n", getpid(), getppid());
-      ret = close(fd[1]);
+      ret = close(fd[i][1]);
       if(ret < 0){
         fprintf(stderr, "Fils (%d) impossible de fermer l'extrémité fd[1] du pipe (%d)\n", getpid(), errno);
         return 1;
       }
-      int ret = read(fd[0], buffer, 1);
+      int ret = read(fd[i][0], buffer, 1);
       if(ret < 0){
         fprintf(stderr, "Fils (%d) impossible de lire sur fd[0] du pipe (%d)\n", getpid(), errno);
         return 1;
@@ -39,10 +39,12 @@ int main(int argc, char *argv[]){
    
     }
   }
-  ret = close(fd[0]);
-  if(ret < 0){
-    fprintf(stderr, "Père (%d) impossible de fermer l'extrémité fd[0] du pipe (%d)\n", getpid(), errno);
-    return 1;
+  for(i = 0; i < 4; i++){
+    ret = close(fd[i][0]);
+    if(ret < 0){
+      fprintf(stderr, "Père (%d) impossible de fermer l'extrémité fd[0] du pipe (%d)\n", getpid(), errno);
+      return 1;
+    }
   }
   char carac;
   char strcarac[2];
@@ -51,10 +53,12 @@ int main(int argc, char *argv[]){
     if(carac == '\n')
       continue;
     sprintf(strcarac, "%c", carac);
-    ret = write(fd[1], strcarac, 1);
-    if(ret < 0){
-      fprintf(stdout, "Père (%d) impossible d'écrire sur fd[1] du pipe (%d)\n", getpid(), errno);
-      return 1;
+    for(i = 0; i < 4; i++){
+      ret = write(fd[i][1], strcarac, 1);
+      if(ret < 0){
+        fprintf(stdout, "Père (%d) impossible d'écrire sur fd[1] du pipe (%d)\n", getpid(), errno);
+        return 1;
+      }
     }
     if(carac == 'N')
       break;
